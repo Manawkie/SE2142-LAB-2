0b7d944ad66e9637cad6d026eff798507d4c8eb6
Ulimer25 <joshuaulimer.demerin-20@cpu.edu.ph>
2024-09-12 03:33:45 +0800

    For cherry-pick

--- three-way-feature-file.py
+++ three-way-feature-file.py
@@ -1,1 +1,6 @@
-Some changes on three-way-feature branch
+# Some changes on three-way-feature branch
+
+def say_hello(name: str, age: int):
+  return f"Hellooooooo! My name is {name} and I am {age} years old!!!!"
+
+print(say_hello('Josh', 21))
