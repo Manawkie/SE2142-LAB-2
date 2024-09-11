83a2473f4864d21fe6cdd82a7fcd936a3d987172
Manawkie <samenianjoshua@gmail.com>
2024-09-11 23:41:38 +0800

    added a new line in samenian_says_something.py

--- samenian_says_something.py
+++ samenian_says_something.py
@@ -1,4 +1,9 @@
 def dogBark(dog_name: str):
     print(f'the name dog named "{dog_name}" barked at you!')
 
+
+def saysSomethingStupid():
+    print("I am stupid hehe")
+
 dogBark("casio")
+saysSomethingStupid()
