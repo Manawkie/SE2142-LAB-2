961099adf02f7a2e1980779ec3fe8e2d4735d676
Rops-11 <jrofer.casio11@gmail.com>
2024-09-11 22:52:01 +0800

    This is for Git Revert.

--- casio_doing_what.py
+++ casio_doing_what.py
@@ -1,5 +1,7 @@
-def doing(activity):
+def doing(activity: str):
   return "I am currently doing {activity}."
 
 print(doing("the function \"doing\" for master branch."))
 print(doing("the changes for the ammending."))
+print(doing("the git restore part."))
+print(doing("the git revert part."))
