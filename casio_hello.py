6a6f0b8dbf60edbbcaf77f531be161ea4eab1978
Rops-11 <jrofer.casio11@gmail.com>
2024-09-11 11:49:20 +0800

    Casio's Day

--- casio_hello.py
+++ casio_hello.py
@@ -1,4 +1,4 @@
-def say_hello(name, age):
+def say_hello(name: str, age: int):
   return f"Hellooooooo! My name is {name} and I am {age} years old!!!!"
 
 print(say_hello('Rofer', 21))
---
+++ casio_myday.py
@@ -0,0 +1,4 @@
+def my_day(feeling: str, rating: int):
+  return f'Today was {feeling}, so I\'d rate my day as a {rating}.'
+
+print(my_day('joyful', 10))
